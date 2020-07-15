23a6a6b96ae8563283d3d699ddde2c73d7078017
HendrikVE <18201166+Hennots@users.noreply.github.com>
2020-07-15 19:42:55 +0200

    solved 70

---
+++ leetcode/70/solution.go
@@ -0,0 +1,16 @@
+package leetcode
+
+// 70. Climbing Stairs
+// You are climbing a stair case.
+// It takes n steps to reach to the top.
+// Each time you can either climb 1 or 2 steps.
+// In how many distinct ways can you climb to the top?
+
+func climbStairs(n int) int {
+	i, j := 1, 1
+	for n > 0 {
+		n--
+		i, j = j, i+j
+	}
+	return i
+}
