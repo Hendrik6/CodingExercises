7f257de3c1f51410893e862cf8680ef7548007b9
HendrikVE <18201166+Hennots@users.noreply.github.com>
2020-07-28 18:42:43 +0200

    solved 206

---
+++ leetcode/206/solution.go
@@ -0,0 +1,25 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	//Making a new node
+	var front *ListNode
+
+	//creating two variables which are both the same head param
+	mid, end := head, head
+
+	//if head isn't nil
+	for mid != nil {
+
+		//set the end as the next node
+		end = mid.Next
+
+		mid.Next = front
+		front, mid = mid, end
+	}
+	return front
+}
