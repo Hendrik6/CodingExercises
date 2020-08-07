5ac7f76c29a5e0f74f823de4e84d589ada282e4f
HendrikVE <18201166+Hennots@users.noreply.github.com>
2020-08-07 18:45:07 +0200

    added comment

--- leetcode/1502/solution.go
+++ leetcode/1502/solution.go
@@ -1,5 +1,7 @@
 package leetcode
 
+// 1502. Can Make Arithmetic Progression From Sequence
+
 // Given an array of numbers arr.
 // A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
 // Return true if the array can be rearranged to form an arithmetic progression, otherwise, return false.
