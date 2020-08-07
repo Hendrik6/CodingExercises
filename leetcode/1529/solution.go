e0e7ca3c6af1a84f489d3c786b41eb588eb1dcfb
HendrikVE <18201166+Hennots@users.noreply.github.com>
2020-08-07 18:43:30 +0200

    Comment

--- leetcode/1529/solution.go
+++ leetcode/1529/solution.go
@@ -1,5 +1,7 @@
 package leetcode
 
+// 1529. Bulb Switcher IV
+
 // There is a room with n bulbs, numbered from 0 to n-1, arranged in a row from left to right. Initially all the bulbs are turned off.
 // Your task is to obtain the configuration represented by target where target[i] is '1' if the i-th bulb is turned on and is '0' if it is turned off.
 // You have a switch to flip the state of the bulb, a flip operation is defined as follows:
