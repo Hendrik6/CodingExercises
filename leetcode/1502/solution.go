89f75e2edce5e451d1290ed9095a0d0ea1dfd076
HendrikVE <18201166+Hennots@users.noreply.github.com>
2020-08-08 17:44:14 +0200

    solved 1502

--- leetcode/1502/solution.go
+++ leetcode/1502/solution.go
@@ -1,14 +1,30 @@
 package leetcode
 
+import "sort"
+
 // 1502. Can Make Arithmetic Progression From Sequence
-//https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
 
 // Given an array of numbers arr.
 // A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
 // Return true if the array can be rearranged to form an arithmetic progression, otherwise, return false.
 
 func canMakeArithmeticProgression(arr []int) bool {
-	//TODO
+	if len(arr) == 1 {
+		return true
+	}
+
+	sort.Ints(arr)
+
+	diff := arr[1] - arr[0]
+
+	for i, j := range arr {
+		if i == 0 {
+			continue
+		}
+		if j-arr[i-1] != diff {
+			return false
+		}
+	}
 
 	return true
 }
